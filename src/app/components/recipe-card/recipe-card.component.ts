80b15f89145f492c9ca1da5ac449faee1fc79e36
maitevargas9 <132816437+maitevargas9@users.noreply.github.com>
2025-10-08 12:08:35 +0200

    Recipe recipe-card calculate ingredients by changing the portions and add to shopping list

--- src/app/components/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-card/recipe-card.component.ts
@@ -1,22 +1,46 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { Recipe } from '../../models/recipe';
 import { Ingredient } from '../../models/ingredient';
 
 @Component({
   selector: 'app-recipe-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './recipe-card.component.html',
   styleUrls: ['./recipe-card.component.scss'],
 })
-export class RecipeCardComponent {
+export class RecipeCardComponent implements OnInit {
   @Input() recipe!: Recipe;
 
   @Output() edit = new EventEmitter<Recipe>();
   @Output() delete = new EventEmitter<Recipe>();
   @Output() toggleFavorite = new EventEmitter<Recipe>();
   @Output() addToShoppingList = new EventEmitter<Ingredient[]>();
+
+  currentPortions: number = 1;
+  baseIngredients: Ingredient[] = [];
+
+  ngOnInit() {
+    this.currentPortions = this.recipe.basePortions || 1;
+    this.baseIngredients = JSON.parse(JSON.stringify(this.recipe.ingredients));
+  }
+
+  onPortionsChange() {
+    if (this.currentPortions < 1) {
+      this.currentPortions = 1;
+    }
+
+    this.recipe.ingredients = this.baseIngredients.map((ing) => ({
+      ...ing,
+      amount: (ing.amount * this.currentPortions) / this.recipe.basePortions,
+    }));
+  }
+
+  onAddToShoppingList() {
+    this.addToShoppingList.emit(this.recipe.ingredients);
+  }
 
   onEdit() {
     this.edit.emit(this.recipe);
@@ -28,10 +52,6 @@ export class RecipeCardComponent {
 
   onToggleFavorite() {
     this.toggleFavorite.emit(this.recipe);
-  }
-
-  onAddToShoppingList() {
-    this.addToShoppingList.emit(this.recipe.ingredients);
   }
 
   getIngredientsText(): string {
